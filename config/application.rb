74059e2951999abb4816eb067347a8236baa31a4
JD Warren <jd@teamairship.com>
2021-12-10 09:54:38 -0600

    allow any host

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,6 @@ module TestPg
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.hosts << "147.182.161.131"
+    # config.hosts << "147.182.161.131"
   end
 end
